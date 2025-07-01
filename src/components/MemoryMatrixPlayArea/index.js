38610f9f96b750ba7059eded8e57d988ccfcedd7
Ganesh <saiganeshpinnam41@gmail.com>
2025-07-01 14:13:46 +0000

    solution

--- src/components/MemoryMatrixPlayArea/index.js
+++ src/components/MemoryMatrixPlayArea/index.js
@@ -1,8 +1,42 @@
 import {Component} from 'react'
 
+import './index.css'
+
 class MemoryMatrixPlayArea extends Component {
+  state = {
+    gridSizes: 3,
+    highlightedBoxes: [],
+    selectedBoxes: [],
+    isClickable: false,
+  }
+
+  renderGridBoxes = () => {
+    const {gridSizes} = this.state
+    let gridBoxesIds = []
+
+    for (let i = 0; i < gridSizes; i++) {
+      for (let j = 0; j < gridSizes; j++) {
+        gridBoxesIds.push(JSON.stringify(i) + JSON.stringify(j))
+      }
+    }
+    return (
+      <ul className="grids-container">
+        {gridBoxesIds.map(eachBox => (
+          <li key={eachBox} className="each-grid-box"></li>
+        ))}
+      </ul>
+    )
+  }
+
   render() {
-    return <h1>play area</h1>
+    const {gridSizes} = this.state
+    console.log(gridSizes)
+
+    return (
+      <div className="memory-play-area-bg-container">
+        {this.renderGridBoxes()}
+      </div>
+    )
   }
 }
 
